9429855d67416089d60a9998698e2a277bc393e8
marchev <n1tmare@gmail.com>
2016-01-14 01:33:59 +0200

    An empty Spring Boot application.

---
+++ src/main/java/bg/obshtestvo/gradame/GradaMeApplication.java
@@ -0,0 +1,12 @@
+package bg.obshtestvo.gradame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradaMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradaMeApplication.class, args);
+	}
+}
